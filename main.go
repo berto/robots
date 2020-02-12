ceba131b9030586c972ab5ed14c4ad12a1c257b3
berto <berto.ort@gmail.com>
2020-02-12 15:04:27 -0700

    post and delete route

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 func main() {
 	r := gin.Default()
 	r.Use(connectDB())
-	r.GET("/robots", handleRobots)
+	r.GET("/robots", getRobotHandler)
+	r.POST("/robots", addRobotHandler)
+	r.DELETE("/robots/:id", deleteRobotHandler)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- robots.go
+++ robots.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
+	uuid "github.com/satori/go.uuid"
 )
 
 // Robot is the model for our db robot
@@ -13,12 +16,79 @@ type Robot struct {
 	Dangerous bool   `json:"dangerous" db:"dangerous"`
 }
 
-func handleRobots(c *gin.Context) {
+// Validate checks to make sure all fields are valid
+func (r Robot) Validate() bool {
+	if r.Name == "" && len(r.Name) < 1000 {
+		return false
+	}
+	if r.Type == "" {
+		return false
+	}
+	return true
+}
+
+func getRobotHandler(c *gin.Context) {
 	db := c.Value("db").(*sqlx.DB)
 	robots := []Robot{}
 	err := db.Select(&robots, "SELECT * FROM robot")
 	if err != nil {
 		c.JSON(500, err.Error())
+		return
 	}
 	c.JSON(200, robots)
+}
+
+func addRobotHandler(c *gin.Context) {
+	// grab the robot from the body of the request
+	robot := Robot{}
+	err := c.BindJSON(&robot)
+	if err != nil {
+		c.JSON(500, err.Error())
+		return
+	}
+	id := uuid.NewV4()
+	robot.ID = id.String()
+	// validate robot information
+	if ok := robot.Validate(); !ok {
+		c.JSON(500, "invalid robot information")
+		return
+	}
+	// insert robot into db
+	db := c.Value("db").(*sqlx.DB)
+	_, err = db.NamedExec(`
+		INSERT INTO
+			robot (id, name, type, dangerous)
+		VALUES (:id,:name,:type,:dangerous);
+		`,
+		&robot,
+	)
+	if err != nil {
+		c.JSON(500, err.Error())
+		return
+	}
+	// respond with a 200 and new id
+	c.JSON(200, robot)
+}
+
+func deleteRobotHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	db := c.Value("db").(*sqlx.DB)
+	result, err := db.Exec(`
+		DELETE FROM robot
+		WHERE id = $1;
+	`,
+		id,
+	)
+	if err != nil {
+		fmt.Println("INSIDE ERR", result)
+		c.JSON(500, err.Error())
+		return
+	}
+	rows, err := result.RowsAffected()
+	if rows < 1 || err != nil {
+		c.JSON(500, "unable to delete")
+		return
+	}
+	c.JSON(200, id)
 }
